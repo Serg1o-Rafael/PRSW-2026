2289b2cabe2b50725f9eb099771e19b8d227d4a4
Serg1o-Rafael <sergiorafaelgithub520@gmail.com>
2026-02-25 19:24:31 -0300

    25/02/26

--- javascript/prog1.js
+++ javascript/prog1.js
@@ -1,1 +1,9 @@
 //Declaração de variáveis
+var a = 10
+var b = 20
+var c = 0
+
+c = a + b
+// comando "console.log" é o terminal do vs code ou do navegador
+// log () é uma função similar a função escreva do Portugol Studio
+console.log("A soma entre as variáveis = " + c)
