ab59d844d3296e95e139824eb08dfe69464432eb
Serg1o-Rafael <sergiorafaelgithub520@gmail.com>
2026-02-25 20:28:11 -0300

    25/02/26

---
+++ javascript/prog2.js
@@ -0,0 +1,13 @@
+//Declaração de variáveis
+
+let valor = 50
+let dobro = 0
+let triplo = 0
+
+dobro = valor * 2
+triplo = valor * 3
+
+console.log("O dobro de " + valor + " = " + dobro)
+
+console.log("O triplo de " + valor + " = " + triplo)
+ 
---
+++ javascript/prog3.js
@@ -0,0 +1,12 @@
+//Declaração de variáveis
+
+let valor, modulo
+
+valor = 10
+modulo = valor % 2 // modulo é o resto da divisão 
+if(modulo == 0) {
+    console.log( valor +" é par!")
+}
+else{
+    console.log(valor + " é impar!" )
+}
