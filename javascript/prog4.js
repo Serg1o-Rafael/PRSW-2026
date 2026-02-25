ee25ba7f38ef210ce19c046525be2aae4eac8e40
Serg1o-Rafael <sergiorafaelgithub520@gmail.com>
2026-02-25 20:30:09 -0300

    25/02/26

---
+++ javascript/prog4.js
@@ -0,0 +1,13 @@
+
+let valor1 = 10
+let valor2 = 10
+
+if (valor1 > valor2) {
+    console.log(valor1 + " é maior do que " + valor2)
+} 
+else if ( valor1 < valor2) {
+    console.log(valor1 + " é menor que " + valor2)
+}
+else {
+    console.log(valor1 + " é igual que " + valor2)◘
+}
---
+++ javascript/prog5.js
@@ -0,0 +1,8 @@
+let valor = 50
+
+if (valor > 50) {
+    console.log(" Parabéns, atingiu a meta!")
+}
+else  {
+    console.log(" Tente novamente! ")
+}
