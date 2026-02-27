b8e6fd08ed222e7fc572dd27106c552ae81b8a4e
Serg1o-Rafael <sergiorafaelgithub520@gmail.com>
2026-02-26 21:13:58 -0300

    26/02/26

--- javascript/prog6.js
+++ javascript/prog6.js
@@ -2,3 +2,4 @@ let valor1 = 9, valor2 = 5
 
 console.log(" A soma dos valores " + parseInt(valor1 + valor2) + "\n A diferença entre valores " + parseInt(valor1 - valor2))
 
+''
---
+++ javascript/prog7.js
@@ -0,0 +1,5 @@
+let valor1 = 8.5 // No padrão mundial, o separador decimal é o ponto
+let valor2 = 5.5
+let valor3 = 7.25
+
+console.log("Média entre os valores: " + parseFloat(valor1+ valor2 + valor3 / 3))
