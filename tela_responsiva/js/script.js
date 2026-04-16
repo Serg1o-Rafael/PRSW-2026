6c12de293eb58fe09ac99c7e5c5f7cfd2c05b662
Serg1o-Rafael <sergiorafaelgithub520@gmail.com>
2026-04-16 19:15:35 -0300

    16/04/26

--- tela_responsiva/js/script.js
+++ tela_responsiva/js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
-    let largura_janela = $(window).width()
-    if(largura_janela <= 480) {
-        $("#paragrafo").append("Esta frase é somente para dispositivo móvel")
-    }  else if(largura_janela >= 481 && largura_janela <= 768) {
-    $("#paragrafo").append("Esta frase é somente para tablet") {
-        else if(largura_)
+    let largura_janela = $(window).width();
+ 
+    if(largura_janela <= 480){
+        $("#paragrafo").append("Esta frase é somente para dispositivo móvel");
     }
+    // Corrigido: de & para &&
+    else if(largura_janela >= 481 && largura_janela <= 768){
+        $("#paragrafo").append("Esta frase é somente para tablet");
     }
-})
+    else if(largura_janela >= 769 && largura_janela <= 1024) {
+        $("#paragrafo").append("Esta frase é monitor de média resolucao ou seja HD");
+    }
+    else {
+        $("#paragrafo").append("Esta frase é monitor de alta resolucao ou seja FullHD");
+    }
+});
+ 
+ 
