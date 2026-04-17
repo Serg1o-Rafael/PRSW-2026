07e841fdb70fe234528590c7abc1cdcc4b58ab50
Serg1o-Rafael <sergiorafaelgithub520@gmail.com>
2026-04-16 21:00:19 -0300

    16/04/26

--- tela_responsiva/js/script.js
+++ tela_responsiva/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     let largura_janela = $(window).width();
  
     if(largura_janela <= 480){
@@ -14,6 +14,30 @@ $(document).ready(function(){
     else {
         $("#paragrafo").append("Esta frase é monitor de alta resolucao ou seja FullHD");
     }
-});
+}); */
  
- 
+/*  JAVASCRIPT ES6 */
+
+/* A função insertAdjacentText é utilizada para inserir texto em uma posição referente a um elemento (tag HTML) */
+
+/* O parâmetro "beforeend" se refere a posição que é dentro da tag, mas no final dela.*/
+
+function atualizaTexto() {
+
+let largura_janela = window.innerWidth // Largura em Pixel da janela do navegador.
+let paragrafo = document.getElementById("paragrafo")
+
+paragrafo.textContent = ""
+
+if(largura_janela <= 480) {
+    paragrafo.insertAdjacentText("beforeend", "Esta frase é somente para dispositivo móvel")
+} else if(largura_janela >= 481 && largura_janela <= 768){
+    paragrafo.insertAdjacentText("beforeend", "Esta frase é somente para tablet")
+} else if(largura_janela >= 769 && largura_janela <= 1024) {
+    paragrafo.insertAdjacentText("beforeend", "Esta frase é monitor de média resolução, ou seja, HD")
+} else {
+    paragrafo.insertAdjacentText("beforeend", "Esta frase é monitor de alta resolução ou seja FullHD");
+}
+        
+}
+window.addEventListener("resize", atualizaTexto)
